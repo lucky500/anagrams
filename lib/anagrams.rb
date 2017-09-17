e47aa708174e6d13571f928e13821d527a187be5
Luciano Oliveira <luciano101@msn.com>
2017-09-17 14:39:22 -0700

    checking for palindrome version 1 working

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,20 +1,40 @@
+require('pry')
+
 class Anagrams
+  def initialize(sentence1, sentence2)
+    @sentence1 = sentence1
+    @sentence2 = sentence2
+  end
+
+  def is_palindrome?(sentence1, sentence2)
+    @sentence1 = sentence1.downcase.scan(/\w/)
+    @sentence2 = sentence2.downcase.scan(/\w/)
+    @sentence1 == @sentence2
+  end
+
   # def is_anagrams?
   #   clean_anagram = self.gsub!(/[^A-Za-z]/, '')
   # end
+  # def is_palindrome?(word)
+  #   palindrome_words = []
+  #   word = word.downcase()
+  #   word = word.split(" ")
+  #   word.each do |i|
+  #     if i == i.reverse
+  #       'are palindrome words'
+  #       palindrome_words.push(i)
+  #   end
+  # end
 
   def get_anagrams(sentence1, sentence2)
-    sentence1 = sentence1.gsub(/[^0-9a-z]/i, '')
-    sentence2 = sentence2.gsub(/[^0-9a-z]/i, '')
-    sentence1 = sentence1.downcase.split("").sort()
-    sentence2 = sentence2.downcase.split("").sort()
+    @sentence1 = sentence1.gsub(/[^0-9a-z]/i, '').downcase.split("").sort()
+    @sentence2 = sentence2.gsub(/[^0-9a-z]/i, '').downcase.split("").sort()
     # if (sentence1 =~ /[0-9]/) || (sentence2 =~ /[0-9]/)
     #   "Please enter a sentence"
-    if sentence1 == sentence2
+    if @sentence1 == @sentence2
       "These two sentences are anagrams"
     else
       "These two sentences are not anagrams"
     end
-
   end
 end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -8,24 +8,32 @@ describe("Anagrams#get_anagrams") do
   # end
 
   it("Program will return an error if first word and second word are not anagrams.") do
-  anagrams = Anagrams.new()
+  anagrams = Anagrams.new("Hello", "Ola")
   expect(anagrams.get_anagrams("Hello", "Ola")).to(eq("These two sentences are not anagrams"))
   end
 
   it("Program will check if two words have the same letters and are anagrams") do
-  anagrams = Anagrams.new()
+  anagrams = Anagrams.new("ruby", "bury")
   expect(anagrams.get_anagrams("ruby", "bury")).to(eq("These two sentences are anagrams"))
   end
 
   it("Program will ignore case sensitivity") do
-  anagrams = Anagrams.new()
+  anagrams = Anagrams.new("Owl", "low")
   expect(anagrams.get_anagrams("Owl", "low")).to(eq("These two sentences are anagrams"))
   end
 
 
   it("Program will check if multiple words are anagrams") do
-  anagrams = Anagrams.new()
+  anagrams = Anagrams.new("Im a Dot in Place", "A Decimal Point")
   expect(anagrams.get_anagrams("Im a Dot in Place", "A Decimal Point")).to(eq("These two sentences are anagrams"))
+   #binding.pry
   end
 
+end
+
+describe("Anagrams#is_palindrome?") do
+  anagrams = Anagrams.new("Anina", "Anina")
+  it("Program will check to see if words in sentence one are palindromes") do
+    expect(anagrams.is_palindrome?("Anina", "Anina")).to(eq(true))
+  end
 end
